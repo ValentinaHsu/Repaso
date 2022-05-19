385ff245b454ecb34d102057c8e93af4fd8030e2
ValentinaHsu <92767924+ValentinaHsu@users.noreply.github.com>
2022-05-19 13:52:07 -0300

    Initial commit

---
+++ Assets/ActivateDeactivate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDeactivate : MonoBehaviour {
+    public GameObject myObject;
+
+	// Use this for initialization
+	void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //if (myObject.activeInHierarchy == true)
+            //{
+            //    myObject.SetActive(false);
+            //}
+            //else
+            //{
+            //    myObject.SetActive(true);
+            //}
+            myObject.SetActive(!myObject.activeInHierarchy);
+            
+        }
+	}
+}
